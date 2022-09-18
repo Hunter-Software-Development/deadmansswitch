d5b809301a2b79aa912bb9440d16be77cf990a81
Andrew Hunter <iamhunter@users.noreply.github.com>
2022-09-18 18:41:22 -0400

    add proper server link

--- apps/client/src/index.js
+++ apps/client/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://ingress-dms-hunter-software-development-deadmansswitch-pr-1.cloud.okteto.net/server',
+  uri: 'https://ingress-dms-hunter-software-development-deadmansswitch-pr-1.cloud.okteto.net/server/graphql',
   cache: new InMemoryCache(),
 });
 
