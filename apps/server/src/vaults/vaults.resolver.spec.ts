d05597da940f446d227fa2516251eae201647e9a
Andrew Hunter <iamhunter@users.noreply.github.com>
2022-10-31 18:56:06 -0400

    add service unit tests

--- apps/server/src/vaults/vaults.resolver.spec.ts
+++ apps/server/src/vaults/vaults.resolver.spec.ts
@@ -5,6 +5,9 @@ import { VaultsService } from './vaults.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { Vault, VaultSchema } from './entities/vault.schema';
 import { CreateVaultInput } from './dto/create-vault.input';
+import { UpdateVaultInput } from './dto/update-vault.input';
+import { GetVaultInput } from './dto/get-vault.input';
+import { RemoveVaultInput } from './dto/remove-vault.input';
 
 describe('VaultsResolver', () => {
   let resolver: VaultsResolver;
@@ -44,6 +47,78 @@ describe('VaultsResolver', () => {
         .mockImplementation(async () => result);
 
       expect(await resolver.createVault(arg)).toBe(result);
+    });
+  });
+
+  describe('update', () => {
+    it('should run update', async () => {
+      const result: Vault = {
+        _id: 'test_id',
+        googleId: 'test_google_id',
+        data: 'test_data',
+      };
+      const arg: UpdateVaultInput = {
+        _id: 'test_id',
+        googleCredential: 'test_google_cred',
+        data: 'test_data',
+      };
+      jest
+        .spyOn(resolver, 'updateVault')
+        .mockImplementation(async () => result);
+
+      expect(await resolver.updateVault(arg)).toBe(result);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should run findAll', async () => {
+      const result: Vault[] = [
+        {
+          _id: 'test_id',
+          googleId: 'test_google_id',
+          data: 'test_data',
+        },
+      ];
+      const arg = 'test_google_cred';
+
+      jest.spyOn(resolver, 'findAll').mockImplementation(async () => result);
+
+      expect(await resolver.findAll(arg)).toBe(result);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should run findOne', async () => {
+      const result: Vault = {
+        _id: 'test_id',
+        googleId: 'test_google_id',
+        data: 'test_data',
+      };
+      const arg: GetVaultInput = {
+        _id: 'test_id',
+      };
+      jest.spyOn(resolver, 'findOne').mockImplementation(async () => result);
+
+      expect(await resolver.findOne(arg)).toBe(result);
+    });
+  });
+
+  describe('removeVault', () => {
+    it('should run removeVault', async () => {
+      const result: Vault = {
+        _id: 'test_id',
+        googleId: 'test_google_id',
+        data: 'test_data',
+      };
+      const arg: RemoveVaultInput = {
+        _id: 'test_id',
+        googleCredential: 'test_google_id',
+      };
+      jest
+        .spyOn(resolver, 'removeVault')
+        .mockImplementation(async () => result);
+
+      expect(await resolver.removeVault(arg)).toBe(result);
     });
   });
 });
