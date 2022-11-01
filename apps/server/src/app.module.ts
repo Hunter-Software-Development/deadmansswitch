09a1a72f6de4a4bb1b7355e3bccf69b0883c3c33
Andrew Hunter <iamhunter@users.noreply.github.com>
2022-10-31 21:21:52 -0400

    tweak some more ig

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -14,6 +15,8 @@ import { VaultsModule } from './vaults/vaults.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: process.cwd() + '/apps/server/src/schema.gql',
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION, {
       dbName: 'deadmansswitch',
